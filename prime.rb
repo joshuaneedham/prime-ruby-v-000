1723f7ea846108cf8e638db08d8b09dc1972efa7
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 17:29:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,12 @@
 # Add  code here!
 def prime?(n)
-prime_array = []    
-p = 2
-if n < 2
+  prime_array = []
+  p = 2
+  if n < 2
     return p
-end
-
-while (n % p == 0) && (p < n)
+  end
+  while (n % p == 0) && (p < n)
     prime_array.push(p)
     p += 1
-end
-#puts prime_array.inspect
-return prime_array
+  end
 end
