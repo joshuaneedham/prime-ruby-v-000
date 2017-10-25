27a6930bf2dbbd30b3fb4eb2af0c7b7f7a9fefc9
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 19:12:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,9 @@
 def prime?(n)
   if n <= 1
     return false
-  elsif (2..n).each do |i|
+  end
+  (2..n).each do |i|
     return false if (n % i == 0)
   end
-  else
-    return true
-  end
+  true
 end
