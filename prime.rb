a8c1389c27ff2fe977fe857c34a80ce1b9d65999
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 18:14:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,17 @@
 # Add  code here!
 def prime?(n)
-    if (n % n == 0)
-      return true
-    else
-      return false
-    end
-end
+  	max = Math.sqrt(n)
+  	for i in 2 .. max
+  		if n%i==0 then
+  			return false
+  		end
+  	end
+  	return n>1
+  end
+
+  for i in 0 .. 100
+  	if isPrime(i) then
+  		puts "#{i} is prime"
+  	end
+  end
+
