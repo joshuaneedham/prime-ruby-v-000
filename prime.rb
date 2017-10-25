f2fbaded0f8c2089596545578d7a90f89bec7ef0
Joshua Needham <joshuabneedham@gmail.com>
2017-10-25 17:24:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,4 @@
 # Add  code here!
-def prime? n
-  (2..Math.sqrt(n)).none? do |f|
-    if n % f == 0 || n % -f == 0
-      return false
-    else
-      return true
-    end
-  end
+def prime?(n)
+  (2..Math.sqrt(n)).none? {|f| n % f == 0 || n % -f == 0 }
 end
